1560c6e53489ebd85982c6ce02a581416cc97bf9
vmqxdv <vquintilio@gmail.com>
2025-03-12 13:50:43 +0100

    snack 8

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,6 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+const mancorsClass = students.find(e => e.name === 'Marco Lanci').class;
+
+console.log(mancorsClass);

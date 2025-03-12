50f577c3cb29c3503c057d57d0cbf5b67be914d4
vmqxdv <vquintilio@gmail.com>
2025-03-12 13:45:20 +0100

    snack 5

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,5 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+const evenNums = nums.filter(e => e % 2 === 0);
+console.log(evenNums);

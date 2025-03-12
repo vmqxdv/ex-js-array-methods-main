7d5f3abf2762cdcb039723ef7eaf37b05b8fe6bd
vmqxdv <vquintilio@gmail.com>
2025-03-12 13:43:34 +0100

    snack 3

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,5 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+const plusOne = numbers.map(e => e + 1);
+console.log(plusOne);
